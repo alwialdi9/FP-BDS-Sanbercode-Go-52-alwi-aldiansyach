173c7d3099da9345d6b3358c948944b57891dffc
alwialdi9 <alwi.aldisyach@gmail.com>
2024-01-02 23:56:51 +0700

    change host

--- config/db.go
+++ config/db.go
@@ -12,7 +12,7 @@ import (
 func ConnectDataBase() *gorm.DB {
 	username := utils.Getenv("MYSQLUSER", "root")
 	password := utils.Getenv("MYSQL_ROOT_PASSWORD", "")
-	host := utils.Getenv("MYSQLHOST", "tcp(127.0.0.1:3306)")
+	host := "viaduct.proxy.rlwy.net:52716"
 	database := utils.Getenv("MYSQL_DATABASE", "restaurant_api")
 
 	dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
