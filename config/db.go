8ddb9a569c9823d9f3a5139b0bf9996b8ca5e2a1
alwialdi9 <alwi.aldisyach@gmail.com>
2024-01-03 00:06:13 +0700

    change host tcp

--- config/db.go
+++ config/db.go
@@ -11,7 +11,7 @@ import (
 func ConnectDataBase() *gorm.DB {
 	username := "root"
 	password := "1d1--63-2gEgfCbDH2adaFeagaG1-b3H"
-	host := "viaduct.proxy.rlwy.net:52716"
+	host := "tcp(viaduct.proxy.rlwy.net:52716)"
 	database := "railway"
 
 	dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
